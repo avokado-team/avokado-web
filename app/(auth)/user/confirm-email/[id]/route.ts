abac8678f4fdb8ec4a03b0bdab69b3d523c752e9
MINT <pb1123love@gmail.com>
2024-11-04 21:18:16 +0900

    ✨ refactor(email-confirmation): update GET method to async
    Converts the GET method to async to properly handle the 
    fetch operation. This change ensures that the request is 
    awaited, improving error handling and control flow during 
    the email confirmation process.

--- app/(auth)/user/confirm-email/[id]/route.ts
+++ app/(auth)/user/confirm-email/[id]/route.ts
@@ -1,14 +1,12 @@
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const id = request.nextUrl.pathname.split("/")[3];
   const api = `${process.env.GATEWAY_ENDPOINT}/user/confirm-email/${id}`;
-  console.log(api);
-  fetch(api)
+  await fetch(api)
     .then((res) => {
       if (res.ok) {
-        console.log(res);
         return redirect("/user/confirm-email-done");
       }
       return redirect("/user/confirm-email-error");
